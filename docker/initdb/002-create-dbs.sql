3b14a48ad0ccb837968386bca8a9ef4d0787cdea
dain <0901ekdls@gmail.com>
2025-11-05 01:01:32 +0900

    feat: DB 초기 설정 및 도커 환경 구성
    - docker-compose.yml 추가
    - docker/initdb/002-create-dbs.sql 추가
    - PostgreSQL 초기 DB 자동 생성 스크립트 반영
    
    #27

---
+++ docker/initdb/002-create-dbs.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE oneforlogis_user;
+CREATE DATABASE oneforlogis_hub;
+CREATE DATABASE oneforlogis_order;
+
+GRANT ALL PRIVILEGES ON DATABASE oneforlogis_user  TO admin;
+GRANT ALL PRIVILEGES ON DATABASE oneforlogis_hub  TO admin;
+GRANT ALL PRIVILEGES ON DATABASE oneforlogis_order TO admin;
