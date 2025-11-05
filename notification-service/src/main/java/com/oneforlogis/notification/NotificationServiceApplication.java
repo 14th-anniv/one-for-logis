620faf0d518f5d279b5fd66e69b6b96e86451ab4
GoodNyong <gy990213@gmail.com>
2025-11-05 18:37:50 +0900

    fix: common confi 적용 및 security 추가

--- notification-service/src/main/java/com/oneforlogis/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/oneforlogis/notification/NotificationServiceApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@Import({
+        com.oneforlogis.common.config.SwaggerConfig.class,
+        com.oneforlogis.common.config.JpaAuditConfig.class
+})
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
