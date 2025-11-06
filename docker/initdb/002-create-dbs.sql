2637a2d9e24891f3012021d4689685a4e6934705
Sona <objet_an@naver.com>
2025-11-06 13:33:55 +0900

    add: init-dbs 파일 내 company 추가
    - company 개발 중에는 다른 데이터 베이스를 주석 처리하고 사용

--- docker/initdb/002-create-dbs.sql
+++ docker/initdb/002-create-dbs.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE oneforlogis_user;
 CREATE DATABASE oneforlogis_hub;
 CREATE DATABASE oneforlogis_order;
+CREATE DATABASE oneforlogis_company;
 
 GRANT ALL PRIVILEGES ON DATABASE oneforlogis_user  TO admin;
 GRANT ALL PRIVILEGES ON DATABASE oneforlogis_hub  TO admin;
