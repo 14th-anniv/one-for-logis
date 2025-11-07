3b7afd7a653553b2232f48e405479bff111c4ec6
Robbie <Robbie@email.com>
2025-11-07 11:55:36 +0900

    feat: 환경 설정 완료

--- docker/initdb/002-create-dbs.sql
+++ docker/initdb/002-create-dbs.sql
@@ -1,7 +1,13 @@
 CREATE DATABASE oneforlogis_user;
 CREATE DATABASE oneforlogis_hub;
 CREATE DATABASE oneforlogis_order;
+CREATE DATABASE oneforlogis_company;
+CREATE DATABASE product_db;
+CREATE DATABASE notification_db;
 
 GRANT ALL PRIVILEGES ON DATABASE oneforlogis_user  TO admin;
 GRANT ALL PRIVILEGES ON DATABASE oneforlogis_hub  TO admin;
 GRANT ALL PRIVILEGES ON DATABASE oneforlogis_order TO admin;
+GRANT ALL PRIVILEGES ON DATABASE oneforlogis_company TO admin;
+GRANT ALL PRIVILEGES ON DATABASE product_db TO admin;
+GRANT ALL PRIVILEGES ON DATABASE notification_db TO admin;
