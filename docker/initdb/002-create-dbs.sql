49b82d64fea2f7744b51673c62fdd1083fc0c8fe
Sona <objet_an@naver.com>
2025-11-10 14:34:40 +0900

    add: 상품 init database 추가

--- docker/initdb/002-create-dbs.sql
+++ docker/initdb/002-create-dbs.sql
@@ -2,6 +2,7 @@ CREATE DATABASE oneforlogis_user;
 CREATE DATABASE oneforlogis_hub;
 CREATE DATABASE oneforlogis_order;
 CREATE DATABASE oneforlogis_company;
+CREATE DATABASE oneforlogis_product;
 
 GRANT ALL PRIVILEGES ON DATABASE oneforlogis_user  TO admin;
 GRANT ALL PRIVILEGES ON DATABASE oneforlogis_hub  TO admin;
