6b484b7963c8e64bfcaed405a1cc2d85ad3e5ca7
Sona <objet_an@naver.com>
2025-11-11 14:16:45 +0900

    fix: 타 모듈 통신을 위해 임시 permitAll 설정

--- company-service/src/main/java/com/oneforlogis/company/global/SecurityConfig.java
+++ company-service/src/main/java/com/oneforlogis/company/global/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configurers.AuthorizeH
 @EnableMethodSecurity
 public class SecurityConfig extends SecurityConfigBase {
 
-    // todo: 기본 설정 사용. 인가 과정 필요 시 configureAuthorization 오버라이딩 후 로직 추가
     @Override
     protected void configureAuthorization(
             AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
--- product-service/src/main/java/com/oneforlogis/product/global/config/SecurityConfig.java
+++ product-service/src/main/java/com/oneforlogis/product/global/config/SecurityConfig.java
@@ -3,21 +3,18 @@ package com.oneforlogis.product.global.config;
 import com.oneforlogis.common.security.SecurityConfigBase;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig extends SecurityConfigBase {
 
-    // todo: 기본 설정 사용. 인가 과정 필요 시 configureAuthorization 오버라이딩 후 로직 추가
-
-    /*
-
+    // 모듈 통신 개발용 주석 제거
     @Override
     protected void configureAuthorization(
             AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
         auth
                 .requestMatchers("/api/v1/**").permitAll();
     }
-
-    */
 }
