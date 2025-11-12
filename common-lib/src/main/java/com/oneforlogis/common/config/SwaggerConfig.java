8a6a4889cf95f142217a407da5eb0c1876153d33
Dayun Kim <ozsally0201@gmail.com>
2025-11-13 03:32:58 +0900

    chore: swagger config 서버 추가 및 헤더 jwt 토큰으로 변환

--- common-lib/src/main/java/com/oneforlogis/common/config/SwaggerConfig.java
+++ common-lib/src/main/java/com/oneforlogis/common/config/SwaggerConfig.java
@@ -1,42 +1,45 @@
 package com.oneforlogis.common.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "One For Logis API", version = "v1"),
-        security = {
-                @SecurityRequirement(name = "X-User-Id"),
-                @SecurityRequirement(name = "X-User-Name"),
-                @SecurityRequirement(name = "X-User-Role")
-        }
-)
-@SecuritySchemes({
-        @SecurityScheme(
-                name = "X-User-Id",
-                type = SecuritySchemeType.APIKEY,
-                in = SecuritySchemeIn.HEADER,
-                paramName = "X-User-Id"
-        ),
-        @SecurityScheme(
-                name = "X-User-Name",
-                type = SecuritySchemeType.APIKEY,
-                in = SecuritySchemeIn.HEADER,
-                paramName = "X-User-Name"
-        ),
-        @SecurityScheme(
-                name = "X-User-Role",
-                type = SecuritySchemeType.APIKEY,
-                in = SecuritySchemeIn.HEADER,
-                paramName = "X-User-Role"
-        )
-})
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
+    @Bean
+    public OpenAPI openAPI() {
+        List<Server> servers = new ArrayList<>();
+        servers.add(0, new Server().url("http://localhost:8000"));
+        servers.add(1, new Server().url("http://gateway-service:8000"));
+
+        return new OpenAPI()
+                .servers(servers)
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("One For Logis API")
+                .description("One For Logis API 입니다.")
+                .version("v1");
+    }
 }
---
+++ gateway-service/src/main/java/com/oneforlogis/gateway/global/cofig/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.oneforlogis.gateway.global.cofig;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("One For Logis API")
+                .description("One For Logis API 입니다.")
+                .version("v1");
+    }
+}
