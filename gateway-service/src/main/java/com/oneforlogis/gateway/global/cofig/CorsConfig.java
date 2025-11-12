1f47605fa64eda393c91b5caf219fb51f17e6245
Dayun Kim <ozsally0201@gmail.com>
2025-11-13 03:35:03 +0900

    chore: cors config 추가

---
+++ gateway-service/src/main/java/com/oneforlogis/gateway/global/cofig/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.oneforlogis.gateway.global.cofig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8000");
+        config.addAllowedOrigin("http://gateway-service:8000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
