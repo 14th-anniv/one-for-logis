523b12f5719bd1285a0c52684ece92ff7306945f
Sona <objet_an@naver.com>
2025-11-13 10:17:55 +0900

    add: internal swagger 태그 작성

--- company-service/src/main/java/com/oneforlogis/company/presentation/controller/internal/CompanyInternalController.java
+++ company-service/src/main/java/com/oneforlogis/company/presentation/controller/internal/CompanyInternalController.java
@@ -3,6 +3,8 @@ package com.oneforlogis.company.presentation.controller.internal;
 import com.oneforlogis.company.application.CompanyService;
 import com.oneforlogis.company.domain.model.Company;
 import com.oneforlogis.company.presentation.controller.internal.dto.CompanyDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Internal companies", description = "내부용 업체 조회 API")
 @RestController
 @RequestMapping("/api/v1/internal/companies")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class CompanyInternalController {
 
     private final CompanyService companyService;
 
+    @Operation(summary = "업체 id로 단일 조회", description = "업체 ID로 단일 업체 정보를 조회합니다.")
     @GetMapping("/{companyId}")
     public CompanyDto getCompanyInternal(@PathVariable UUID companyId) {
         Company company = companyService.getCompanyById(companyId);
--- product-service/src/main/java/com/oneforlogis/product/presentation/controller/internal/ProductInternalController.java
+++ product-service/src/main/java/com/oneforlogis/product/presentation/controller/internal/ProductInternalController.java
@@ -3,6 +3,8 @@ package com.oneforlogis.product.presentation.controller.internal;
 import com.oneforlogis.product.application.ProductService;
 import com.oneforlogis.product.domain.model.Product;
 import com.oneforlogis.product.presentation.controller.internal.dto.ProductDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Internal products", description = "내부용 상품 API")
 @RestController
 @RequestMapping("/api/v1/internal/products")
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ public class ProductInternalController {
     private final ProductService productService;
 
     // 상품 조회
+    @Operation(summary = "상품 id로 단일 조회", description = "상품 ID로 단일 상품 정보를 조회합니다.")
     @GetMapping("/{productId}")
     public ProductDto getProductInternal(@PathVariable UUID productId){
         Product product = productService.getProductById(productId);
@@ -28,6 +32,7 @@ public class ProductInternalController {
     }
 
     // 재고 감소
+    @Operation(summary = "상품 재고 차감", description = "주어진 상품 ID에 대해 재고를 지정한 수량만큼 차감합니다.")
     @PostMapping("/{productId}/decrease-stock")
     public ResponseEntity<ProductDto> decreaseStock(
             @PathVariable UUID productId,
@@ -38,6 +43,7 @@ public class ProductInternalController {
     }
 
     // 재고 증가
+    @Operation(summary = "상품 재고 증가", description = "주어진 상품 ID에 대해 재고를 지정한 수량만큼 증가시킵니다. ")
     @PostMapping("/{productId}/increase-stock")
     public ResponseEntity<ProductDto> increaseStock(
             @PathVariable UUID productId,
